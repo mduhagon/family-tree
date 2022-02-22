89f64b327eab1413ed9870c4e4c7bb138dd84802
Maria Duhagon <mduhagon@gmail.com>
2022-02-22 22:41:33 +0100

    first version

---
+++ tree-data/tree.js
@@ -0,0 +1,13 @@
+let familyTree = [
+    {"key":1, "name":"Juan Duhagon", "birth": "1/12/1833 - Iholdy (Francia)", "marriage": "casamiento: ? - Bs As", "spouse": "Margarita Mertinez"},
+    {"key":2, "name":"Bernardo Duhagon","parent":1, "birth": "? - Bs As", "marriage": "casamiento: ? - Chacabuco", "spouse": "Maria Polonia Marquinez"},
+    {"key":3, "name":"Juan Duhagon", "parent":2, "birth": "1885 - Chacabuco", "marriage": "", "spouse": "Juana Dolores Fluttaz"},
+    {"key":4, "name":"Fernando Duhagon", "parent":2, "birth": "1886 - Chacabuco", "marriage": "", "spouse": ""},
+    {"key":5, "name":"Margarita Duhagon", "parent":2, "birth": "1888 - Chacabuco", "marriage": "", "spouse": "Santiago Fluttaz"},
+    {"key":6, "name":"Maria Balbina Duhagon", "parent":2, "birth": "1889 - Chacabuco", "marriage": "", "spouse": ""},
+    {"key":7, "name":"Gregorio Duhagon", "parent":2, "birth": "1891 - Chacabuco", "marriage": "", "spouse": "María Sharry"},
+    {"key":8, "name":"Maria Duhagon", "parent":2, "birth": "1892 - Chacabuco", "marriage": "", "spouse": ""},
+    {"key":9, "name":"Bernardo Duhagon", "parent":2, "birth": "1893 - Chacabuco", "marriage": "", "spouse": ""},
+    {"key":10, "name":"Pedro Duhagon", "parent":2, "birth": "6/8/1896 - Chacabuco", "marriage": "casamiento: 25/4/1925 - Chacabuco", "spouse": "Maria Polonia Carricart"},
+    {"key":11, "name":"Horacio Duhagon", "parent":10, "birth": "1936 - Junin", "marriage": "", "spouse": "Elida M. Balocco"},
+]
